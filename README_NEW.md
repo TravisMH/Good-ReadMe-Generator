67d9d495dad1f61c266530c74d70aa6397fee317
Travis Hill <travismhill@gmail.com>
2020-10-25 11:51:55 -0400

    backend

---
+++ README_NEW.md
@@ -0,0 +1,32 @@
+
+# good readme generator 
+lots of back end
+* [Installation](#Installation)
+* [Instructions](#Instructions)
+* [License](#License)
+* [Author](#Author)
+    
+## Installation
+```npm i, npm i inquirer, npm i axios```
+
+## Use
+```node index.js```
+
+## Example
+```Easily create readmes for github profile```
+
+## License
+The license for this project MIT 
+    
+## Author
+**TravisMH**
+
+Travis Hill
+
+
+
+![ProfileImage](https://avatars1.githubusercontent.com/u/8617885?v=4)
+
+Email: Unprovided
+
+GitHub: https://github.com/TravisMH
--- index.js
+++ index.js
@@ -3,43 +3,94 @@ const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 
-var userInput = await inquirer.prompt([
-    {
-        type: "input",
-        message: "GitHub username?",
-        name: "username",
-    },
-    {
-        type: "input",
-        message: "How would you describe this project?",
-        name: "projectdescription",
-    },
-    {
-        type: "input",
-        message: "What were the steps taken to install the project?",
-        name: "install",
-    },
-    {
-        type: "input",
-        message: "What command do you run to start the ReadMe Generator?",
-        name: "command",
-    },
-    {
-        type: "input",
-        message: "What license did you use?",
-        name: "license",
-    },
-    {
-        type: "input",
-        message: "What would be a good example of use?",
-        name: "use",
-    },
-    {
-        type: "input",
-        message: "Press Enter to generate ReadMe",
-        name: "start",
-    },
-]);
+async function main() {
+    const prompts = [
+        {
+            type: 'input',
+            message: "GitHub username?",
+            name: "username",
+        },
+        {
+            type: 'input',
+            message: "What is the name of your project",
+            name: "projectName",
+        },
+        {
+            type: 'input',
+            message: "How would you describe this project?",
+            name: "projectDescription",
+        },
+        {
+            type: 'input',
+            message: "What were the steps taken to install the project?",
+            name: "install",
+        },
+        {
+            type: 'input',
+            message: "What command do you run to start the ReadMe Generator?",
+            name: "command",
+        },
+        {
+            type: 'input',
+            message: "What license did you use? (Default: UNLICENSED)",
+            name: "license",
+        },
+        {
+            type: 'input',
+            message: "What would be a good example of use?",
+            name: "use",
+        },
+        {
+            type: 'input',
+            message: "Press Enter to generate ReadMe",
+            name: "start",
+        },
+    ];
+    const userResponse = await inquirer.prompt(prompts);
 
-console.log('starting');
+    const username = userResponse.username || 'travismh';
 
+    const gitResponse = await axios
+        .get(`https://api.github.com/users/${username}`)
+        .catch((err) => console.log(err));
+    console.dir(gitResponse.data)
+    const gitData = gitResponse.data;
+
+    const result = `
+# ${userResponse.projectName || 'my project'} 
+${userResponse.projectDescription || 'my description'}
+* [Installation](#Installation)
+* [Instructions](#Instructions)
+* [License](#License)
+* [Author](#Author)
+    
+## Installation
+\`\`\`${userResponse.install || 'npm install'}\`\`\`
+
+## Use
+\`\`\`${userResponse.command || 'npm start'}\`\`\`
+
+## Example
+\`\`\`${userResponse.use || ' '}\`\`\`
+
+## License
+The license for this project ${userResponse.license || 'UNLICENSED'} 
+    
+## Author
+**${gitData.login || 'Unknown'}**
+
+${gitData.name || 'Name'}
+
+${gitData.location || ''}
+
+![ProfileImage](${gitData.avatar_url})
+
+Email: ${gitData.email || 'Unprovided'}
+
+GitHub: ${gitData.html_url || ''}
+`;
+    const writePath = path.join(__dirname, './', 'README_NEW.md');
+    fs.writeFileSync(writePath, result);
+    console.log(`Your readme.md has been generated and can be accessed at ${writePath}`);
+}
+main();
